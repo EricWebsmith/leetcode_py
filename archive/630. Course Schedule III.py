4129c79293532f39ce86722cb8fa6344eb42de9a
Eric Websmith <eric.websmith@gmail.com>
2022-06-24 22:37:07 +0800

    630. Course Schedule III

---
+++ archive/630. Course Schedule III.py
@@ -0,0 +1,38 @@
+from heapq import heappop, heappush
+import unittest
+from typing import List
+
+class Solution:
+    def scheduleCourse(self, courses: List[List[int]]) -> int:
+        courses.sort(key=lambda x:x[1])
+        heap = []
+        max_time = 0
+        for time, end_time in courses:
+            heappush(heap, -time)
+            max_time += time
+            if max_time > end_time:
+                big_time = heappop(heap)
+                max_time += big_time
+        return len(heap)
+
+def test(testObj: unittest.TestCase, courses:List[List[int]], expected:int) -> None:
+    s = Solution()
+    actual = s.scheduleCourse(courses)
+    testObj.assertEqual(actual, expected)
+    
+class TestStringMethods(unittest.TestCase):
+
+    def test_1(self):
+        test(self, [[100,200],[200,1300],[1000,1250],[2000,3200]], 3)
+        
+    def test_2(self):
+        test(self, [[1,2]], 1)
+
+    def test_3(self):
+        test(self, [[3,2],[4,3]], 0)
+
+if __name__ == '__main__':
+    unittest.main()
+
+#Runtime: 741 ms, faster than 91.08% of Python3 online submissions for Course Schedule III.
+#Memory Usage: 20.2 MB, less than 29.29% of Python3 online submissions for Course Schedule III.
