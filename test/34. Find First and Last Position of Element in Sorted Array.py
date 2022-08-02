1ab3c9c5b45e7e562b558db01100df7773f0254c
Eric Websmith <eric.websmith@gmail.com>
2022-08-02 15:08:50 +0800

    34

---
+++ test/34. Find First and Last Position of Element in Sorted Array.py
@@ -0,0 +1,44 @@
+from bisect import bisect_left, bisect_right
+from turtle import right
+from typing import List
+import unittest
+
+
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+        if n == 0:
+            return [-1, -1]
+        left = bisect_left(nums, target)
+        # print(left)
+        if left == len(nums) or nums[left] != target:
+            return [-1, -1]
+
+        right = bisect_right(nums, target) - 1
+        return [left, right]
+
+
+def test(testObj: unittest.TestCase, cardPoints: List[int], k: int, expected: int) -> None:
+    s = Solution()
+    actual = s.searchRange(cardPoints, k)
+    testObj.assertEqual(actual, expected)
+
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_1(self):
+        test(self, [5, 7, 7, 8, 8, 10], 8, [3, 4])
+
+    def test_2(self):
+        test(self, [5, 7, 7, 8, 8, 10], 6, [-1, -1])
+
+    def test_3(self):
+        test(self, [], 0, [-1, -1])
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+# Runtime: 83 ms, faster than 98.27% of Python3 online submissions for Find First and Last Position of Element in Sorted Array.
+# Memory Usage: 15.4 MB, less than 92.49% of Python3 online submissions for Find First and Last Position of Element in Sorted Array.
