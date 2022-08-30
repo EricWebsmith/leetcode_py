16f820bfaadbc423ec7e99a7cf0097a563261b5e
Eric Websmith <eric.websmith@gmail.com>
2022-08-31 00:31:08 +0800

    887 Super Egg Drop

--- test/lc_887_super_egg_drop.py
+++ test/lc_887_super_egg_drop.py
@@ -7,37 +7,21 @@ from data_structure.nary_tree import Node, array_to_node, node_to_array
 
 class Solution:
     def superEggDrop(self, k: int, n: int) -> int:
-        if k == 1:
-            return n
+        """
+        dp(i,j) with i eggs and j allowed steps, the max floor one can test to certain f
+        dp(i, j) = dp(i - 1, j - 1) + 1 + dp(i, j - 1)
+        dp(0, j) = 0
+        dp(i, 0) = 0
+        min j that makes dp(k, j) >= n 
+        """
+        memo = [[0]*(n + 1) for _ in range(k + 1)]
         
-        dp = list(range(0, n+1))
-        for kk in range(2, k+1):
-            next_dp = [0]
-            prev_first_drop = 1
-            for i in range(1, n+1):
-                if i == 1 or i == 2:
-                    next_dp.append(i)
-                else:
-                    # first drop = previous first_drop
-                    first_drop = prev_first_drop
-                    first_break = 1 + dp[first_drop-1]
-                    first_not_break = 1 + next_dp[i - first_drop]
-                    plan_1 = max(first_break, first_not_break)
-
-                    first_drop = prev_first_drop + 1
-                    first_break = 1 + dp[first_drop-1]
-                    first_not_break = 1 + next_dp[i - first_drop]
-                    plan_2 = max(first_break, first_not_break)
-
-                    if plan_2<=plan_1:
-                        next_dp.append(plan_2)
-                        prev_first_drop = prev_first_drop + 1
-                    else:
-                        next_dp.append(plan_1)
-
-            dp = next_dp
-
-        return dp[n]
+        for j in range(1, n + 1):
+            for i in range(1, k + 1):
+                memo[i][j] = memo[i - 1][j - 1] + 1 + memo[i][j - 1]
+                if i == k and memo[i][j] >= n:
+                    return j
+        return -1
 
 
 def test(testObj: unittest.TestCase, k: int, n: int, expected:int) -> None:
@@ -126,6 +110,6 @@ if __name__ == '__main__':
     unittest.main()
 
 '''
-Runtime: 3374 ms, faster than 9.20% of Python3 online submissions for Super Egg Drop.
-Memory Usage: 14.4 MB, less than 74.30% of Python3 online submissions for Super Egg Drop.
+Runtime: 65 ms, faster than 80.11% of Python3 online submissions for Super Egg Drop.
+Memory Usage: 20.4 MB, less than 68.11% of Python3 online submissions for Super Egg Drop.
 '''
