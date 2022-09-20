0bf5e18dbfd6d9d738b816b777f7b015a8090daf
Eric Websmith <eric.websmith@gmail.com>
2022-09-20 22:01:41 +0800

    715. Range Module

--- test/lc_715_range_module.py
+++ test/lc_715_range_module.py
@@ -8,52 +8,51 @@ from collections import deque, defaultdict
 from data_structure.binary_tree import TreeNode, array_to_treenode, treenode_to_array
 from data_structure.nary_tree import Node, array_to_node, node_to_array
 null = None
-
+true = True
+false = False
 
 class RangeModule:
 
     def __init__(self):
-        # we treat ranges as time spans
-        # because range is a python keyword.
-        self.spans = []
+        self.arr = []
 
     def addRange(self, left: int, right: int) -> None:
-        index = bisect_left(self.spans, x=left, key=lambda x: x[0])
-        # merge with next
-        if index < len(self.spans) and right >= self.spans[index][0]:
-            right = self.spans[index][1]
-            del self.spans[index]
-        if index > 0 and left <= self.spans[index-1][1]:
-            left = self.spans[index-1][0]
-            del self.spans[index-1]
-            index -= 1
-
-        self.spans.insert(index, (left, right))
+        # Main Logic 
+        #   If idx(left) or idx(right) is odd, it's in a interval. So don't add it. 
+        #   If idx(left) or idx(right) is even, it's not in any interval. So add it as new interval 
+        #   Slice array[idx(left) : idx(right)]
+        #       1) both odd: Nothing is added. Merge all middle intervals. 
+        #       2) both even: Add new intervals. Merge all middle intervals
+        #       3) idx(left) is even: update start point of next interval with left
+        #       4) idx(right) is even: update end point of previous interval with right
+        # Bisect_left vs. Bisect_right
+        #   left need to proceed all interval closing at left, so use Bisect_left
+        #   right need to go after all interval openning at right, so use Bisect_right
+        i, j = bisect_left(self.arr, left), bisect_right(self.arr, right)
+        self.arr[i : j] = [left] * (i % 2 == 0) + [right] * (j % 2 == 0)
+        
 
     def queryRange(self, left: int, right: int) -> bool:
-        index = bisect_right(self.spans, x=left, key=lambda x: x[0])
-        if index == 0:
-            return False
-        if self.spans[index-1][1] >= right:
-            return True
-
-        return False
+        # Main logic 
+        #   If idx of left/right is odd, it's in a interval. Else it's not. 
+        #   If idx of left&right is the same, they're in the same interval
+        # Bisect_left vs. Bisect_right
+        #   [start, end). Start is included. End is not. 
+        #   so use bisect_right for left 
+        #   so use bisect_left for right 
+        i, j = bisect_right(self.arr, left), bisect_left(self.arr, right)
+        return i == j and i % 2 == 1
+        
 
     def removeRange(self, left: int, right: int) -> None:
-        left_index = bisect_right(self.spans, x=left, key=lambda x: x[1])
-        right_index = bisect_right(self.spans, x=right, key=lambda x: x[0])
-        if left_index+1 == right_index:
-            span1 = (self.spans[left_index][0], left)
-            span2 = (right, self.spans[left_index][1])
-            del self.spans[left_index]
-            self.spans.insert(left_index, span2)
-            self.spans.insert(left_index, span1)
-            return
-        self.spans = self.spans[0:left_index+1] + self.spans[right_index-1:]
-        if self.spans[left_index][1] > left:
-            self.spans[left_index] = (self.spans[left_index][0], left)
-        if left_index+1 < len(self.spans) and self.spans[left_index+1][0] < right:
-            self.spans[left_index+1] = (right, self.spans[left_index+1][1])
+        # Main Logic 
+        #   If idx(left) is odd, the interval that contains left need to change end point to left 
+        #   If idx(right) is odd, the interval that contains right need to change start point to right
+        #   Else, everything from idx(left) to idx(right) is removed. Nothing is changed. 
+        # Bisect_left vs. Bisect_right
+        #   Same as addRange
+        i, j = bisect_left(self.arr, left), bisect_right(self.arr, right)
+        self.arr[i : j] = [left] * (i % 2 == 1) + [right] * (j % 2 == 1)
 
 
 def test(testObj: unittest.TestCase, actions: List, params: List, expected: List) -> None:
@@ -90,10 +89,16 @@ class TestClass(unittest.TestCase):
         test(self, ["RangeModule", "addRange", "addRange", "removeRange", "queryRange", "queryRange", "queryRange"], [
              [], [10, 20], [30, 40], [15, 35], [10, 14], [13, 15], [16, 17]], [None, None, None, None, True, True, False])
 
+    def test_3(self):
+        test(self, ["RangeModule","addRange","removeRange","removeRange","addRange","removeRange","addRange","queryRange","queryRange","queryRange"],
+[[],[6,8],[7,8],[8,9],[8,9],[1,3],[1,8],[2,4],[2,9],[4,6]],
+[null,null,null,null,null,null,null,true,true,true])
+
 
 if __name__ == '__main__':
     unittest.main()
 
 '''
-
+Runtime: 384 ms, faster than 95.08% of Python3 online submissions for Range Module.
+Memory Usage: 18.6 MB, less than 61.51% of Python3 online submissions for Range Module.
 '''
