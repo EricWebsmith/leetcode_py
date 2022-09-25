118f334026103666f35bf9b56ee2b9d090ae573c
Eric Websmith <eric.websmith@gmail.com>
2022-09-26 01:06:11 +0800

    2421. Number of Good Paths

---
+++ temp/lc_6189.py
@@ -0,0 +1,47 @@
+from heapq import heappop, heappush
+import unittest
+from typing import List
+
+class Solution:
+    def longestSubarray(self, nums: List[int]) -> int:
+        n = len(nums)
+        max_value = max(nums)
+
+        ans = 1
+        current = 0
+        for i in range(n):
+            if nums[i] == max_value:
+                current+=1
+            else:
+                ans = max(ans, current)
+                current = 0
+        ans = max(ans, current)
+        return ans
+
+def test(testObj: unittest.TestCase, nums:List[int], expected:int) -> None:
+    s = Solution()
+    actual = s.longestSubarray(nums)
+    testObj.assertEqual(actual, expected)
+    
+class TestStringMethods(unittest.TestCase):
+
+    def test_1(self):
+        test(self, [1,2,3,3,2,2], 2)
+        
+    def test_2(self):
+        test(self, [1,2,3,4], 1)
+
+    def test_3(self):
+        test(self, [1,2,3,4,5], 1)
+
+    def test_4(self):
+        test(self, [1,2,3,4,4,4,3,2,1], 3)
+
+    def test_5(self):
+        test(self, [378034,378034,378034], 3)
+
+if __name__ == '__main__':
+    unittest.main()
+
+#Runtime: 741 ms, faster than 91.08% of Python3 online submissions for Course Schedule III.
+#Memory Usage: 20.2 MB, less than 29.29% of Python3 online submissions for Course Schedule III.
---
+++ test/lc_2421_number_of_good_paths.py
@@ -0,0 +1,88 @@
+import unittest
+from typing import List, Optional, Dict, Set, Any
+from collections import defaultdict, Counter
+
+
+class DSU:
+    def __init__(self, n: int) -> None:
+        self.p = list(range(n))
+        self.e = 0
+
+    def find(self, x: int) -> int:
+        if x != self.p[x]:
+            self.p[x] = self.find(self.p[x])
+        return self.p[x]
+
+    def union(self, x: int, y: int) -> int:
+        px = self.find(x)
+        py = self.find(y)
+        if px == py:
+            return 1
+
+        if px > py:
+            self.p[px] = py
+        else:
+            self.p[py] = px
+        self.e += 1
+        return 0
+
+class Solution:
+    def numberOfGoodPaths(self, vals: List[int], edges: List[List[int]]) -> int:
+        n = len(vals)
+        uf = DSU(n)
+        e = defaultdict(list)
+        for u, v in edges:
+            e[u].append(v)
+            e[v].append(u)
+        
+        value_indices = defaultdict(list)
+        for i, v in enumerate(vals):
+            value_indices[v].append(i)
+
+        ans = 0
+        for v in sorted(value_indices.keys()):
+            for i in value_indices[v]:
+                for j in e[i]:
+                    if vals[j] <= v:
+                        uf.union(i, j)
+
+            c = Counter()
+            for i in value_indices[v]:
+                p = uf.find(i)
+                c[p] += 1
+
+            for _, count in c.items():
+                if count == 1:
+                    ans += 1
+                else:
+                    ans += count * (count + 1) // 2
+
+        return ans
+
+
+
+def test(testObj: unittest.TestCase, vals: List[int], edges: List[List[int]], expected:int) -> None:
+    so = Solution()
+    actual = so.numberOfGoodPaths(vals,edges)
+    testObj.assertEqual(actual, expected)
+
+
+class TestClass(unittest.TestCase):
+    
+    def test_1(self):
+        test(self,   [1,3,2,1,3],  [[0,1],[0,2],[2,3],[2,4]], 6)
+
+    def test_2(self):
+        test(self,   [1,1,2,2,3],  [[0,1],[1,2],[2,3],[2,4]], 7)
+
+    def test_3(self):
+        test(self,   [1],  [], 1)
+    
+
+if __name__ == '__main__':
+    unittest.main()
+
+'''
+Runtime: 2031 ms, faster than 69.23% of Python3 online submissions for Number of Good Paths.
+Memory Usage: 31.9 MB, less than 38.46% of Python3 online submissions for Number of Good Paths.
+'''
