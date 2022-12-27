d0acd904deb4c20ed0800260185ebf3864ded50b
Eric Websmith <eric.websmith@gmail.com>
2022-12-27 09:22:29 +0800

    2279. Maximum Bags With Full Capacity of Rocks

---
+++ archive/lc_2279_maximum_bags_with_full_capacity_of_rocks.py
@@ -0,0 +1,46 @@
+import unittest
+from typing import List
+
+
+class Solution:
+    def maximumBags(self, capacity: List[int], rocks: List[int], additionalRocks: int) -> int:
+        spaces: List[int] = [c-r for c, r in zip(capacity, rocks)]
+        spaces.sort()
+        ans = 0
+        for space in spaces:
+            if additionalRocks >= space:
+                additionalRocks -= space
+                ans += 1
+            else:
+                break
+
+        return ans
+
+
+def test(testObj: unittest.TestCase, capacity: List[int], rocks: List[int], additionalRocks: int, expected: int) -> None:
+
+    so = Solution()
+
+    actual = so.maximumBags(capacity, rocks, additionalRocks)
+
+    testObj.assertEqual(actual, expected)
+
+
+class TestClass(unittest.TestCase):
+
+    def test_1(self):
+        test(self,   [2, 3, 4, 5],  [1, 2, 4, 4],  2, 3)
+
+    def test_2(self):
+        test(self,   [10, 2, 2],  [2, 2, 0],  100, 3)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+'''
+Runtime
+928 ms
+Beats
+96.38%
+'''
