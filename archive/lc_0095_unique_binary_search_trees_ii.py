f90a09bf5078e18a63c3deea412ab8ba7efdca75
Eric Websmith <eric.websmith@gmail.com>
2023-08-05 11:45:39 +0800

    95. Unique Binary Search Trees II

--- archive/lc_0095_unique_binary_search_trees_ii.py
+++ archive/lc_0095_unique_binary_search_trees_ii.py
@@ -1,63 +1,51 @@
 import unittest
-
 from leetcode_data_structure import TreeNode
 
 
 class Solution:
     def generateTrees(self, n: int) -> list[TreeNode | None]:
-        def dfs(start, end):
-            n = end - start + 1
-            if n <= 0:
+
+        def generate(left, right):
+            if left == right:
+                return [TreeNode(left)]
+            if left > right:
                 return [None]
-            if n == 1:
-                return [TreeNode(start)]
 
-            ans: list = []  # type: ignore
-            for i in range(start, end + 1):
-                lefts = dfs(start, i - 1)
-                rights = dfs(i + 1, end)
-                for left in lefts:
-                    for right in rights:
-                        node = TreeNode(i)
-                        node.left = left
-                        node.right = right
-                        ans.append(node)
-            return ans
+            res = []
+            for val in range(left, right+1):
 
-        return dfs(1, n)
+                for left_tree in generate(left, val-1):
+                    for right_tree in generate(val+1, right):
+                        root = TreeNode(val, left_tree, right_tree)
+                        res.append(root)
+
+            return res
+        return generate(1, n)
 
 
-def test(testObj: unittest.TestCase, n: int, expected: list[TreeNode | None]) -> None:
+def test(testObj: unittest.TestCase, n: int, expected_matrix: list[list[int | None]]) -> None:
     so = Solution()
-    trees = so.generateTrees(n)
-    actual = [TreeNode.to_array(t) for t in trees]
-    actual.sort()
-    expected.sort()
-    testObj.assertEqual(actual, expected)
+    actual = so.generateTrees(n)
+    expected = [TreeNode.from_array(expected_arr) for expected_arr in expected_matrix]
+    actual_str_list = [TreeNode.to_array(node) for node in actual]
+    expected_str_list = [TreeNode.to_array(node) for node in expected]
+    testObj.assertCountEqual(actual_str_list, expected_str_list)
 
 
 class TestClass(unittest.TestCase):
+
     def test_1(self):
-        test(
-            self,
-            3,
-            [
-                [1, None, 2, None, 3],
-                [1, None, 3, 2],
-                [2, 1, 3],
-                [3, 1, None, None, 2],
-                [3, 2, None, 1],
-            ],
-        )
+        test(self,   3, [[1, None, 2, None, 3], [1, None, 3, 2], [2, 1, 3], [3, 1, None, None, 2], [3, 2, None, 1]])
 
     def test_2(self):
-        test(self, 1, [[1]])
+        test(self,   1, [[1]])
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
 
-"""
-Runtime: 53 ms, faster than 98.29%
-Memory Usage: 15.7 MB, less than 76.03%
-"""
+
+'''
+61ms
+Beats 85.76%of users with Python3
+'''
